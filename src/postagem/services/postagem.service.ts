ffe6c99bb3cbcb16b6935c75c6bbe3323b200dda
Juliana <lealjubs7@gmail.com>
2025-06-23 17:26:41 -0300

    Início do módulo Postagem

---
+++ src/postagem/services/postagem.service.ts
@@ -0,0 +1,20 @@
+import { Repository } from "typeorm";
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Postagem } from "../entities/postagem.entity";
+
+
+@Injectable()
+export class PostagemService {
+    constructor(
+        @InjectRepository(Postagem)
+        private PostagemRepository: Repository<Postagem>,
+
+    ){}
+
+
+async findAll(): Promise<Postagem[]> {
+    return await this.PostagemRepository.find();
+}
+}
+
